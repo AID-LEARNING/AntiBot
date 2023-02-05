2a5da468b8b7b182a093334a578ed9a348a47506
Sensei Tarzan <gabrielcaptari0@gmail.com>
2023-02-05 19:36:43 +0100

    fix

--- src/SenseiTarzan/AntiBot/Listener/BlackListListener.php
+++ src/SenseiTarzan/AntiBot/Listener/BlackListListener.php
@@ -7,19 +7,10 @@ use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
 use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;
-use pocketmine\Server;
-use SenseiTarzan\AntiBot\Component\BlackListManager;
 use SenseiTarzan\ExtraEvent\Class\EventAttribute;
 
 class BlackListListener
 {
-    #[EventAttribute(EventPriority::LOWEST)]
-    public function onPreLogin(PlayerPreLoginEvent $event): void{
-        if (BlackListManager::getInstance()->isBlackListed($event->getPlayerInfo()->getExtraData()['DeviceId'])){
-            $event->setKickReason(PlayerPreLoginEvent::KICK_REASON_PLUGIN, "T'inquete pas mon reuf");
-            $event->setAuthRequired(true);
-        }
-    }
     #[EventAttribute(EventPriority::LOWEST)]
     public function onDataReceive(DataPacketReceiveEvent $event): void{
         $networkSession = $event->getOrigin();
@@ -27,7 +18,6 @@ class BlackListListener
         if ($packet instanceof InventoryTransactionPacket){
             if (count($packet->trData->getActions()) > 100){
                 $event->cancel();
-                BlackListManager::getInstance()->setBlackList($networkSession->getPlayerInfo()->getExtraData()['DeviceId']);
                 (function(){
                     $this->actions = [];
                 })->call($packet->trData);
@@ -35,25 +25,23 @@ class BlackListListener
                 (function(){
                     $this->requestChangedSlots = [];
                 })->call($packet);
-                Server::getInstance()->getNetwork()->blockAddress($networkSession->getIp(),3.154e+7);
+                //Server::getInstance()->getNetwork()->blockAddress($networkSession->getIp(),3.154e+7);
             }
         }
         if ($packet instanceof PlayerAuthInputPacket){
             if ($packet->getBlockActions() !== null && count($packet->getBlockActions()) > 100){
                 $event->cancel();
-                BlackListManager::getInstance()->setBlackList($networkSession->getPlayerInfo()->getExtraData()['DeviceId']);
                 (function(){
                     $this->blockActions = [];
                 })->call($packet);
-                Server::getInstance()->getNetwork()->blockAddress($networkSession->getIp(),3.154e+7);
+                //Server::getInstance()->getNetwork()->blockAddress($networkSession->getIp(),3.154e+7);
             }
             if ($packet->getItemInteractionData() !== null && count($packet->getItemInteractionData()->getRequestChangedSlots()) > 100){
                 $event->cancel();
-                BlackListManager::getInstance()->setBlackList($networkSession->getPlayerInfo()->getExtraData()['DeviceId']);
                 (function(){
                     $this->itemInteractionData = null;
                 })->call($packet);
-                Server::getInstance()->getNetwork()->blockAddress($networkSession->getIp(),3.154e+7);
+                //Server::getInstance()->getNetwork()->blockAddress($networkSession->getIp(),3.154e+7);
             }
         }
     }
--- src/SenseiTarzan/AntiBot/Main.php
+++ src/SenseiTarzan/AntiBot/Main.php
@@ -3,17 +3,11 @@
 namespace SenseiTarzan\AntiBot;
 
 use pocketmine\plugin\PluginBase;
-use SenseiTarzan\AntiBot\Component\BlackListManager;
 use SenseiTarzan\AntiBot\Listener\BlackListListener;
 use SenseiTarzan\ExtraEvent\Component\EventLoader;
 
 class Main extends PluginBase
 {
-
-    protected function onLoad(): void
-    {
-        new BlackListManager($this);
-    }
 
     protected function onEnable(): void
     {
