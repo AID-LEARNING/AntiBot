a200b1a2e8e789616a4d00803840f56018fd03d3
Sensei Tarzan <gabrielcaptari0@gmail.com>
2023-06-29 13:20:33 +0200

    add new BlackList

--- src/SenseiTarzan/AntiBot/Listener/BlackListListener.php
+++ src/SenseiTarzan/AntiBot/Listener/BlackListListener.php
@@ -8,9 +8,12 @@ use pocketmine\network\mcpe\protocol\ClientCacheBlobStatusPacket;
 use pocketmine\network\mcpe\protocol\CraftingEventPacket;
 use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
 use pocketmine\network\mcpe\protocol\ItemStackRequestPacket;
+use pocketmine\network\mcpe\protocol\MobArmorEquipmentPacket;
+use pocketmine\network\mcpe\protocol\MobEquipmentPacket;
 use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;
 use pocketmine\network\mcpe\protocol\SetActorDataPacket;
 use pocketmine\network\mcpe\protocol\TextPacket;
+use pocketmine\network\mcpe\protocol\types\inventory\ItemStackWrapper;
 use pocketmine\Server;
 use SenseiTarzan\ExtraEvent\Class\EventAttribute;
 
@@ -27,6 +30,8 @@ class BlackListListener
     const MAX_CRAFTING_OUTPUT = 1;
     const MAX_HIT_HASHES = 0; //NOT USE by PMMP4
     const MAX_MISS_HASHES = 0; //NOT USE by PMMP4
+    const MAX_ITEM_CAN_DESTROY = 0; //NOT USE by PMMP
+    const MAX_ITEM_CAN_PLACE = 0; //NOT USE by PMMP
 
     #[EventAttribute(EventPriority::LOWEST)]
     public function onDataReceive(DataPacketReceiveEvent $event): void
@@ -35,6 +40,18 @@ class BlackListListener
         if ($packet instanceof InventoryTransactionPacket) {
             if (count($packet->trData->getActions()) >= self::MAX_INVENTORY_TRANSACTION) {
                 $this->blockEvent($event);
+                return;
+            }
+            foreach ($packet->trData->getActions() as $action) {
+                if (
+                    count(($itemOldStack = $action->oldItem->getItemStack())->getCanDestroy()) > self::MAX_ITEM_CAN_DESTROY
+                    || count(($itemNewStack = $action->newItem->getItemStack())->getCanDestroy()) > self::MAX_ITEM_CAN_DESTROY
+                    || count($itemOldStack->getCanPlaceOn()) > self::MAX_ITEM_CAN_PLACE
+                    || count($itemNewStack->getCanPlaceOn()) > self::MAX_ITEM_CAN_PLACE
+                ) {
+                    $this->blockEvent($event);
+                    break;
+                }
             }
         } else if ($packet instanceof PlayerAuthInputPacket) {
             if (($packet->getBlockActions() !== null && count($packet->getBlockActions()) >= self::MAX_BLOCK_ACTION) || ($packet->getItemInteractionData() !== null && count($packet->getItemInteractionData()->getRequestChangedSlots()) >= self::MAX_ITEM_INTERACTION)) {
@@ -51,6 +68,16 @@ class BlackListListener
         } else if ($packet instanceof CraftingEventPacket) {
             if (count($packet->input) > self::MAX_CRAFTING_INPUT || count($packet->output) > self::MAX_CRAFTING_OUTPUT) {
                 $this->blockEvent($event);
+                return;
+            }
+            /**
+             * @var $itemStackWrapper ItemStackWrapper
+             */
+            foreach (array_merge($packet->input, $packet->output) as $itemStackWrapper) {
+                if (count(($itemStack = $itemStackWrapper->getItemStack())->getCanDestroy()) > self::MAX_ITEM_CAN_DESTROY || count($itemStack->getCanPlaceOn()) > self::MAX_ITEM_CAN_PLACE) {
+                    $this->blockEvent($event);
+                    break;
+                }
             }
         } else if ($packet instanceof TextPacket) {
             if (count($packet->parameters) >= self::MAX_TEXT_PARAMETERS) {
@@ -58,6 +85,19 @@ class BlackListListener
             }
         } else if ($packet instanceof ClientCacheBlobStatusPacket) {
             if (count($packet->getHitHashes()) > self::MAX_HIT_HASHES || count($packet->getMissHashes()) > self::MAX_MISS_HASHES) {
+                $this->blockEvent($event);
+            }
+        } else if ($packet instanceof MobArmorEquipmentPacket) {
+            foreach (["head", "chest", "legs", "feet"] as $slot) {
+                if (count(($itemStack = $packet->{$slot}->getItemStack())->getCanDestroy()) > self::MAX_ITEM_CAN_DESTROY
+                    || count($itemStack->getCanPlaceOn()) > self::MAX_ITEM_CAN_PLACE) {
+                    $this->blockEvent($event);
+                    break;
+                }
+            }
+        }else if ($packet instanceof MobEquipmentPacket) {
+            if (count(($itemStack = $packet->item->getItemStack())->getCanDestroy()) > self::MAX_ITEM_CAN_DESTROY
+                || count($itemStack->getCanPlaceOn()) > self::MAX_ITEM_CAN_PLACE) {
                 $this->blockEvent($event);
             }
         }
